61a1827ee9fddfa70f1c8996a2a1f5f716300480
ctrl-root <muriithidennis340@gamil.com>
2023-05-13 10:07:30 +0300

    code update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-   <div>Hello Airbnb!! clone</div>
+   <div className="text-rose-500 text-2xl">Hello Airbnb!! clone</div>
   )
 }
